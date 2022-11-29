3861795e6d6448567220cbad0dc6549f6a84a6f8
Juyeon703 <108377616+Juyeon703@users.noreply.github.com>
2022-11-29 19:35:15 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner keyboardInput = new Scanner(System.in);
+    int a, b;
+    a = keyboardInput.nextInt();
+    b = keyboardInput.nextInt();
+    System.out.println(a + b);
+  }
+}

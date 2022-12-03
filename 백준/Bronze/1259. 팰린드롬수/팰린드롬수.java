f7bcab036b1b48cadbd6f48c94eccd216f901bcc
Juyeon703 <108377616+Juyeon703@users.noreply.github.com>
2022-12-03 21:25:39 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    while (true) {
+       String str = br.readLine();
+      if (str.equals("0")) break;
+      Boolean bl = true;
+      for (int i = 0; i < str.length()/2; i++) {
+        if (str.charAt(i) != str.charAt(str.length() - i - 1)) bl = false;
+      }
+      if (bl == true) sb.append("yes").append('\n');
+      else sb.append("no").append('\n');
+    }
+    System.out.println(sb.toString());
+  }
+}

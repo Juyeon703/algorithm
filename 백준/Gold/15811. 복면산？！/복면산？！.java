c1c50cee7d9b7c10d328f6323f5f36a03e2c2fe9
Juyeon703 <108377616+Juyeon703@users.noreply.github.com>
2023-01-04 18:11:51 +0900

    [Gold IV] Title: 복면산?!, Time: 600 ms, Memory: 19348 KB -BaekjoonHub

--- 백준/Gold/15811. 복면산？！/복면산？！.java
+++ 백준/Gold/15811. 복면산？！/복면산？！.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
   static String str1, str2, str3;
   static int[] abc;
-  static HashSet<Character> set;
+  static List<Character> list;
   static boolean result = false;
 
   public static void main(String[] args) throws IOException {
@@ -22,41 +19,31 @@ public class Main {
     else {
       abc = new int[26];
       Arrays.fill(abc, -1);
-      int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-      int[] output = new int[set.size()];
       boolean[] visited = new boolean[10];
-      permutation(arr, output, visited, 0, set.size());
+      permutation(visited, 0);
       if (result) System.out.println("YES");
       else System.out.println("NO");
     }
   }
 
-  static void verification(int[] output) {
-    Iterator<Character> iterator = set.iterator();
-    for (int i = 0; i < set.size(); i++) {
-      abc[iterator.next() - 'A'] = output[i];
-    }
-    if (calc(str1) + calc(str2) == calc(str3)) result = true;
-//    System.out.println("result = " + result);
-  }
-
   // 순열 구하기
   // n개 중에서 r개를 순서있게 뽑기
-  static void permutation(int[] arr, int[] output, boolean[] visited, int depth, int r) {
+  static void permutation(boolean[] visited, int depth) {
     if (!result) { // result가 ture가 된 순간 멈춤
-      if (depth == r) {
-//        for (int a : output) {
-//          System.out.print(a);
+      if (depth == list.size()) {
+//        for (int a: abc) {
+//          System.out.print(a + " ");
 //        }
 //        System.out.println();
-        verification(output);
+        if (calc(str1) + calc(str2) == calc(str3)) result = true;
+//        System.out.println("result = " + result);
         return;
       }
       for (int i = 0; i < 10; i++) {
         if (!visited[i]) {
           visited[i] = true;
-          output[depth] = arr[i];
-          permutation(arr, output, visited, depth + 1, r);
+          abc[list.get(depth) - 'A'] = i;
+          permutation(visited, depth + 1);
           visited[i] = false;
         }
       }
@@ -75,16 +62,16 @@ public class Main {
   }
 
   static int distinct() {
-    set = new HashSet<>();
+    list = new ArrayList<>();
     for (int i = 0; i < str1.length(); i++) {
-      set.add(str1.charAt(i));
+      if (!list.contains(str1.charAt(i))) list.add(str1.charAt(i));
     }
     for (int i = 0; i < str2.length(); i++) {
-      set.add(str2.charAt(i));
+      if (!list.contains(str2.charAt(i))) list.add(str2.charAt(i));
     }
     for (int i = 0; i < str3.length(); i++) {
-      set.add(str3.charAt(i));
+      if (!list.contains(str3.charAt(i))) list.add(str3.charAt(i));
     }
-    return set.size();
+    return list.size();
   }
 }

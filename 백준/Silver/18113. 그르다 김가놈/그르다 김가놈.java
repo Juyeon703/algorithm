398a8ab97c076978a39c59bf0c678bd94c42a08b
Juyeon703 <108377616+Juyeon703@users.noreply.github.com>
2023-02-04 23:06:33 +0900

    [Silver II] Title: 그르다 김가놈, Time: 600 ms, Memory: 93580 KB -BaekjoonHub

--- 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
+++ 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
@@ -13,17 +13,23 @@ public class Main {
     int k = Integer.parseInt(st.nextToken()); // 꼬다리 길이
     int m = Integer.parseInt(st.nextToken()); // 김밥조각 최소 개수
     List<Integer> list = new ArrayList<>();
+    int max = 0;
+
     for (int i = 0; i < n; i++) {
       int value = Integer.parseInt(br.readLine());
-
-      if (value > (2 * k))
-        list.add(value-(2*k));
-      if(2*k > value && value>k)
-        list.add(value-k);
+        
+      if (value > (2 * k)) {
+        list.add(value - (2 * k));
+        max = Math.max(max, value - (2*k));
+      }
+      if(2*k > value && value>k) {
+        list.add(value - k);
+        max = Math.max(max, value - k);
+      }
     }
 
     int start = 1;
-    int end = 1000000000;
+    int end = max;
     int result = -1;
 
     while (end - start >= 0) {

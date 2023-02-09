a167b3e353fcf3b57f36c1f97cf20af8c3a5fcb5
Juyeon703 <108377616+Juyeon703@users.noreply.github.com>
2023-02-09 23:00:48 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 400 ms, Memory: 15536 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < 2*(n-i)-1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
